6a60cae8017bab4bda8fc5dc1f2532c49f1b64b3
Nasuschnyi <91079054+Nasuschnyi@users.noreply.github.com>
2024-10-14 14:32:21 +0300

    loading

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Loader = () => {
-	return <section className="loader"></section>;
+	return (
+		<article className="loading">
+			<div className="loading__wrap">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</article>
+	);
 };
 
 export default Loader;
